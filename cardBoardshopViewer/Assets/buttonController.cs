2a4ddd449be870324c8f91c905af7d5512bd377d
Aldian1 <Aldian@hotmail.co.uk>
2016-11-03 00:58:01 +0000

    Just playing with UI and building the color changing shoes

---
+++ cardBoardshopViewer/Assets/buttonController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class buttonController : MonoBehaviour {
+    public Color[] colors;
+
+    public Material[] Materials;
+
+    private Renderer rend;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    
+
+    public void gazeHovertrue(Image button)
+    {
+        button.CrossFadeColor(colors[1], .3F,false,false);
+    }
+
+    public void gazeHoverfalse(Image button)
+    {
+        button.CrossFadeColor(colors[0], .3F, false, false);
+    }
+
+    public void gazeClicked()
+    {
+        changeColor();
+    }
+
+    protected void changeColor()
+    {
+
+    }
+}
