cec1bd019df76f5c1b6fed124bebd7411bca1eab
Aldian <Aldian@hotmail.co.uk>
2016-11-03 00:13:00 +0000

    Library Sessions

---
+++ cardBoardshopViewer/Assets/UI_Controller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UI_Controller : MonoBehaviour {
+
+    public GameObject[] mainUI;
+    void Start()
+    {
+        mainUI = GameObject.FindGameObjectsWithTag("mainUI");
+
+        setUIalpha();   
+    }
+    public void initMainui()
+    {
+        Debug.Log("Event");
+        foreach(GameObject uiElement in mainUI)
+        {
+            uiElement.GetComponent<Image>().CrossFadeAlpha(255F, 5, false);
+        }
+    }
+
+    protected void setUIalpha()
+    {
+        foreach (GameObject uiElement in mainUI)
+        {
+            uiElement.GetComponent<Image>().CrossFadeAlpha(1, 1, false);
+        }
+    }
+}
